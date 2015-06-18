4949fd752c7d934ab955c6bc27b5d4126bc0a587
0x55aa <a0x55aa@foxmail.com>
2015-06-18 15:08:28 +0800

    server write-body write-header

--- server.lisp
+++ server.lisp
@@ -1,24 +1,42 @@
 (in-package :duang)
 
-(duang:get-logger server-logger)
+(get-logger server-logger)
 
 
-(defun write-headers ()
-  ;; 
-  )
-
-
-(defun write-body ()
+(defmacro write-socket-new-line (socket str &rest args)
   ;;
-  )
+  `(as:write-socket-data socket (format nil (concatenate 'string ,str "~C~C")
+                                        ,@args #\Return #\Linefeed)))
+
+
+(defun write-headers (socket headers status-code status-text)
+  "
+  ;; write HTTP headers
+  ;; socket - connection
+  ;; headers - hash table
+  "
+  (write-socket-new-line socket "HTTP/1.1 ~A ~A"
+                         status-code status-text)
+  (maphash #'(lambda (k v)
+               (write-socket-new-line socket "~A: ~A" k v))
+           headers)
+  (write-socket-new-line socket ""))
+
+
+(defun write-body (socket data)
+  ;;
+  (as:write-socket-data socket data))
 
 
 (defun read-callback (socket data)
   ;; 
   (format t "data: ~a~%" data)
-  (as:write-socket-data socket (format nil "HTTP/1.1 200 OK~C~CServer: SBCL-Dung/0.1~C~C~C~CHello World!" #\Return #\Linefeed #\Return #\Linefeed #\Return #\Linefeed)
-                        :write-cb (lambda (socket)
-                                    (as:close-socket socket))))
+  (defparameter h (make-hash-table))
+  (setf (gethash '|Server| h) "SBCL-Duang/0.1")
+  (setf (gethash '|Content-Length| h) "11")
+  (write-headers socket h 200 "OK")
+  (defparameter body-data "Hello World")
+  (write-body socket body-data))
 
 
 (defun event-callback (ev)
@@ -34,7 +52,7 @@
 (defun start-server (&optional (port 8000) (address "0.0.0.0"))
   ;; start server
   (format t "Starting server at ~a:~a~%Quit the server with CONTROL-C.~%" address port)
-  (server-logger :info "~%starting~%")
+  (server-logger :info "starting server")
   (as:with-event-loop ()
     (as:tcp-server address port
                    'read-callback
